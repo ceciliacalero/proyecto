eac791a9f3569ec3c5f1ae749d0478f66b2c875b
Dominguez Calero <c.dominguez.calero@accenture.com>
2019-11-25 16:41:49 +0100

    r

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-insert into beer(name, abv) values('Welcome','Mike');
+insert into beer(name, abv) values('Welcome','Santander');
 insert into beer(name, abv) values('ACN','ACN-Santander');
+
 
 COMMIT;
