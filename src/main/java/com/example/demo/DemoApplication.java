a76b757a6abfd126d409a3fb01dc29899685f504
Dominguez Calero <c.dominguez.calero@accenture.com>
2019-11-26 11:26:06 +0100

    change resources

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,25 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//public class DemoApplication {
-public class UserController {
+public class DemoApplication {
 
-  // DI code omitted
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
-  @RequestMapping
-  public String showUsers(Model model, HttpServletRequest request) {
-
-    int page = Integer.parseInt(request.getParameter("page"));
-    int pageSize = Integer.parseInt(request.getParameter("pageSize"));
-
-    Pageable pageable = new PageRequest(page, pageSize);
-
-    model.addAttribute("users", userService.getUsers(pageable));
-    return "users";
-  }
 }
-//	public static void main(String[] args) {
-//		SpringApplication.run(DemoApplication.class, args);
-//	}
-
-//}
